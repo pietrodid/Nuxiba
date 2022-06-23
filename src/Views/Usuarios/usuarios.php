378d376bbaaead1ee4d8d31ef461cea235695151
pietro <dichiarad@hotmail.com>
2022-06-23 18:25:23 -0400

    Modal for users

--- src/Views/Usuarios/usuarios.php
+++ src/Views/Usuarios/usuarios.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+ <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 <head>
   <meta charset="UTF-8">
-  <title>Mini Framework - Pietro</title>
+  <title>Pietro</title>
 </head>
 <body>
     <?php
@@ -11,9 +14,43 @@
       
     ?>
     <div>
-      <?= $data['usuarios'][$i]['name']; ?>
-      </br>    
+          <?= $data['usuarios'][$i]['name']; ?>  
+        <button type="button" class="btn btn-light" data-toggle="modal" data-target="#exampleModalCenter">º</button>
+      </br> 
     </div>
     <?php } ?>
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <?php
+
+            $selecData = $data['usuarios']; 
+            print_r($selecData);?>
+           
+          
+          <div class="modal-body">
+              <?= $selecData[$i]['name']; ?>  
+          </div>
+      
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
